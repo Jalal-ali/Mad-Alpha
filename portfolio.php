c5772d025fbb819fca9766fce80a5402a42edb82
Jalal Ali <alikhanumar795@gmail.com>
2025-07-17 02:42:45 +0500

    images added

--- portfolio.php
+++ portfolio.php
@@ -2,20 +2,12 @@
 <html class="no-js" lang="zxx">
 
 <head>
-  <meta charset="utf-8" />
-  <meta http-equiv="x-ua-compatible" content="ie=edge" />
-  <title>Bame - Esports & Gaming HTML Template - Tournament</title>
-  <meta name="author" content="Bame" />
-  <meta name="description" content="Bame - Esports & Gaming HTML Template" />
-  <meta name="keywords" content="Bame - Esports & Gaming HTML Template" />
-  <meta name="robots" content="INDEX,FOLLOW" />
-<link rel="icon" href="assets/img/feature/" type="image/png">
-
-  <meta name="msapplication-TileColor" content="#ffffff" />
-  <!-- <meta
-    name="msapplication-TileImage"
-    content="assets/img/favicons/ms-icon-144x144.png" /> -->
-  <meta name="theme-color" content="#ffffff" />
+    <meta charset="UTF-8" />
+  <link rel="icon" type="image/svg+xml" href="assets/logo/imgi_1_LOGO.png" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- Author Info -->
+  <meta name="author" content="Mad Alpha" />
+  <title>Mad-α | Portfolio</title>
   <link rel="preconnect" href="https://fonts.googleapis.com/" />
   <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
   <link
@@ -127,6 +119,7 @@
       flex: 1 1 100%;
       padding: 0 7px;
       margin: 4px auto;
+      
     }
 
     .tab-wrapper:hover .dropdown {
@@ -675,117 +668,10 @@
     </div>
   </section>
 
-  <footer class="footer-wrapper footer-layout1">
-    <div class="container">
-      <div class="footer-top text-center">
-        <div
-          class="footer-logo bg-repeat"
-          data-bg-src="assets/img/bg/jiji-bg.png">
-          <a href="index.php"><span
-              data-mask-src="assets/img/logo.svg"
-              class="logo-mask"></span>
-            <img src="assets/img/logo.svg" alt="Bame" /></a>
-        </div>
-        <div class="footer-links">
-          <ul>
-            <li><a href="index.php">HOME</a></li>
-            <li><a href="about.html">ABOUT US</a></li>
-            <li><a href="tournament.html">TOURNAMENT</a></li>
-            <li><a href="blog.html">BLOG</a></li>
-            <li><a href="gallery.html">GALLERY</a></li>
-            <li><a href="contact.html">CONTACT</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div class="container">
-      <div class="widget-area">
-        <div class="row justify-content-between">
-          <div class="col-md-6 col-lg-auto">
-            <div class="widget footer-widget">
-              <h3 class="widget_title">
-                Office <span class="text-theme">Location:</span>
-              </h3>
-              <div class="th-widget-contact">
-                <div class="info-box">
-                  <p class="info-box_text">
-                    Floor 2, 4 Naval St, Ancoats, Manchester M4 6EW, United
-                    Kingdom
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-auto">
-            <div class="widget footer-widget">
-              <h3 class="widget_title">
-                Contact <span class="text-theme">Information:</span>
-              </h3>
-              <div class="th-widget-contact">
-                <div class="info-box">
-                  <p class="info-box_text">
-                    <a class="info-box_link" href="tel:163254736587">(+163)-2547-36587</a>
-                    <a
-                      class="info-box_link"
-                      href="mailto:infohelp@bameexample.com">infohelp@bameexample.com</a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-auto">
-            <div class="widget footer-widget">
-              <h3 class="widget_title">
-                Follow <span class="text-theme">With Us:</span>
-              </h3>
-              <div class="th-widget-contact">
-                <div class="th-social style-mask">
-                  <a class="facebook" href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
-                  <a class="twitter" href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a>
-                  <a class="instagram" href="https://www.instagram.com/"><i class="fab fa-instagram"></i> </a><a class="linkedin" href="https://www.linkedin.com/"><i class="fab fa-linkedin"></i></a>
-                  <a class="google-play" href="https://www.google.com/"><img
-                      src="assets/img/icon/google-playstore-icon.svg"
-                      alt="icon" /></a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row justify-content-center">
-        <div class="col-lg-8">
-          <form class="newsletter-form">
-            <div class="form-group">
-              <input
-                class="form-control"
-                type="email"
-                placeholder="Subscribe to Our Newsletter"
-                required="" />
-              <button type="submit" class="th-btn">
-                SUBSCRIBE NOW <i class="far fa-arrow-right ms-2"></i>
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-    <div
-      class="copyright-wrap text-center bg-repeat"
-      data-bg-src="assets/img/bg/jiji-bg.png">
-      <div class="container">
-        <div class="row align-items-center justify-content-center">
-          <div class="col-lg-6">
-            <p
-              class="copyright-text bg-repeat"
-              data-bg-src="assets/img/bg/jiji-bg.png">
-              <i class="fal fa-copyright"></i> Copyright 2024
-              <a href="index.php">Bame.</a> All Rights Reserved.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
+    <!-- footer  -->
+  <?php
+  include 'footer.php';
+  ?>
   <div class="scroll-top">
     <svg
       class="progress-circle svg-content"
--- testimonials.php
+++ testimonials.php
@@ -3,78 +3,12 @@
 <!-- Mirrored from html.themeholy.com/bame/demo/team.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 24 Jun 2025 19:32:38 GMT -->
 
 <head>
-  <meta charset="utf-8" />
-  <meta http-equiv="x-ua-compatible" content="ie=edge" />
-  <title>Bame - Esports & Gaming HTML Template - PLAYERS</title>
-  <meta name="author" content="Bame" />
-  <meta name="description" content="Bame - Esports & Gaming HTML Template" />
-  <meta name="keywords" content="Bame - Esports & Gaming HTML Template" />
-  <meta name="robots" content="INDEX,FOLLOW" />
-  <meta
-    name="viewport"
-    content="width=device-width,initial-scale=1,shrink-to-fit=no" />
-  <link
-    rel="apple-touch-icon"
-    sizes="57x57"
-    href="assets/img/favicons/apple-icon-57x57.png" />
-  <link
-    rel="apple-touch-icon"
-    sizes="60x60"
-    href="assets/img/favicons/apple-icon-60x60.png" />
-  <link
-    rel="apple-touch-icon"
-    sizes="72x72"
-    href="assets/img/favicons/apple-icon-72x72.png" />
-  <link
-    rel="apple-touch-icon"
-    sizes="76x76"
-    href="assets/img/favicons/apple-icon-76x76.png" />
-  <link
-    rel="apple-touch-icon"
-    sizes="114x114"
-    href="assets/img/favicons/apple-icon-114x114.png" />
-  <link
-    rel="apple-touch-icon"
-    sizes="120x120"
-    href="assets/img/favicons/apple-icon-120x120.png" />
-  <link
-    rel="apple-touch-icon"
-    sizes="144x144"
-    href="assets/img/favicons/apple-icon-144x144.png" />
-  <link
-    rel="apple-touch-icon"
-    sizes="152x152"
-    href="assets/img/favicons/apple-icon-152x152.png" />
-  <link
-    rel="apple-touch-icon"
-    sizes="180x180"
-    href="assets/img/favicons/apple-icon-180x180.png" />
-  <link
-    rel="icon"
-    type="image/png"
-    sizes="192x192"
-    href="assets/img/favicons/android-icon-192x192.png" />
-  <link
-    rel="icon"
-    type="image/png"
-    sizes="32x32"
-    href="assets/img/favicons/favicon-32x32.png" />
-  <link
-    rel="icon"
-    type="image/png"
-    sizes="96x96"
-    href="assets/img/favicons/favicon-96x96.png" />
-  <link
-    rel="icon"
-    type="image/png"
-    sizes="16x16"
-    href="assets/img/favicons/favicon-16x16.png" />
-  <link rel="manifest" href="assets/img/favicons/manifest.json" />
-  <meta name="msapplication-TileColor" content="#ffffff" />
-  <meta
-    name="msapplication-TileImage"
-    content="assets/img/favicons/ms-icon-144x144.png" />
-  <meta name="theme-color" content="#ffffff" />
+    <meta charset="UTF-8" />
+  <link rel="icon" type="image/svg+xml" href="assets/logo/imgi_1_LOGO.png" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- Author Info -->
+  <meta name="author" content="Mad Alpha" />
+  <title>Mad-α | Testimonials</title>
   <link rel="preconnect" href="https://fonts.googleapis.com/" />
   <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
   <link
@@ -88,12 +22,14 @@
       width: 100%;
       /* Default for mobile */
       border-radius: 40px;
+      margin: 0 auto !important;
     }
 
     /* Desktop styles (applies when screen is 992px or wider) */
     @media (min-width: 992px) {
       .responsive-width {
         width: 70% !important;
+        margin: 0 auto !important;
       }
     }
 
@@ -101,6 +37,7 @@
     @media (min-width: 768px) and (max-width: 991px) {
       .responsive-width {
         width: 85%;
+        margin: 0 auto !important;
       }
     }
 
@@ -536,118 +473,10 @@
     </div>
   </section>
 
-  <!-- footer  -->
-  <footer class="footer-wrapper footer-layout1">
-    <div class="container">
-      <div class="footer-top text-center">
-        <div
-          class="footer-logo bg-repeat"
-          data-bg-src="assets/img/bg/jiji-bg.png">
-          <a href="index.php"><span
-              data-mask-src="assets/img/logo.svg"
-              class="logo-mask"></span>
-            <img src="assets/img/logo.svg" alt="Bame" /></a>
-        </div>
-        <div class="footer-links">
-          <ul>
-            <li><a href="index.php">HOME</a></li>
-            <li><a href="about.html">ABOUT US</a></li>
-            <li><a href="tournament.html">TOURNAMENT</a></li>
-            <li><a href="blog.html">BLOG</a></li>
-            <li><a href="gallery.html">GALLERY</a></li>
-            <li><a href="contact.html">CONTACT</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div class="container">
-      <div class="widget-area">
-        <div class="row justify-content-between">
-          <div class="col-md-6 col-lg-auto">
-            <div class="widget footer-widget">
-              <h3 class="widget_title">
-                Office <span class="text-theme">Location:</span>
-              </h3>
-              <div class="th-widget-contact">
-                <div class="info-box">
-                  <p class="info-box_text">
-                    Floor 2, 4 Naval St, Ancoats, Manchester M4 6EW, United
-                    Kingdom
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-auto">
-            <div class="widget footer-widget">
-              <h3 class="widget_title">
-                Contact <span class="text-theme">Information:</span>
-              </h3>
-              <div class="th-widget-contact">
-                <div class="info-box">
-                  <p class="info-box_text">
-                    <a class="info-box_link" href="tel:163254736587">(+163)-2547-36587</a>
-                    <a
-                      class="info-box_link"
-                      href="mailto:infohelp@bameexample.com">infohelp@bameexample.com</a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-auto">
-            <div class="widget footer-widget">
-              <h3 class="widget_title">
-                Follow <span class="text-theme">With Us:</span>
-              </h3>
-              <div class="th-widget-contact">
-                <div class="th-social style-mask">
-                  <a class="facebook" href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
-                  <a class="twitter" href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a>
-                  <a class="instagram" href="https://www.instagram.com/"><i class="fab fa-instagram"></i> </a><a class="linkedin" href="https://www.linkedin.com/"><i class="fab fa-linkedin"></i></a>
-                  <a class="google-play" href="https://www.google.com/"><img
-                      src="assets/img/icon/google-playstore-icon.svg"
-                      alt="icon" /></a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row justify-content-center">
-        <div class="col-lg-8">
-          <form class="newsletter-form">
-            <div class="form-group">
-              <input
-                class="form-control"
-                type="email"
-                placeholder="Subscribe to Our Newsletter"
-                required="" />
-              <button type="submit" class="th-btn">
-                SUBSCRIBE NOW <i class="far fa-arrow-right ms-2"></i>
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-    <div
-      class="copyright-wrap text-center bg-repeat"
-      data-bg-src="assets/img/bg/jiji-bg.png">
-      <div class="container">
-        <div class="row align-items-center justify-content-center">
-          <div class="col-lg-6">
-            <p
-              class="copyright-text bg-repeat"
-              data-bg-src="assets/img/bg/jiji-bg.png">
-              <i class="fal fa-copyright"></i> Copyright 2024
-              <a href="index.php">Bame.</a> All Rights Reserved.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
+   <!-- footer  -->
+  <?php
+  include 'footer.php';
+  ?>
   <div class="scroll-top">
     <svg
       class="progress-circle svg-content"
